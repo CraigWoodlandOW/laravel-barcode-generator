3d92d68bb9bf1e8ed5b5baeda91b50203a68c112
Craig Woodland <craig@outstandingweb.co.uk>
2018-07-13 12:54:00 +0000

    Bug Fixes

--- src/Utilities/Converters.php
+++ src/Utilities/Converters.php
@@ -1,11 +1,103 @@
 <?php
 
+/*
+    The below BSD licensing statement is copied from the original works, and applies
+    to the logic used to generate the barcodes (everything within the 'create' function)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+    Copyright 2011-2012 Cool Dude 2k - http://idb.berlios.de/
+    Copyright 2011-2012 Game Maker 2k - http://intdb.sourceforge.net/
+    Copyright 2011-2012 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+    $FileInfo: validate.php - Last Update: 02/13/2012 Ver. 2.2.5 RC 1 - Author: cooldude2k $
+*/
+
 namespace CraigWoodlandOW\LaravelBarcodeGenerator\Utilities;
 
 use CraigWoodlandOW\LaravelBarcodeGenerator\Utilities\Validators;
 
 class Converters
 {
+    function convert_upce_to_upca($upc)
+    {
+        if (!isset($upc)||!is_numeric($upc)) {
+            return false;
+        }
+
+        if (strlen($upc)==7) {
+            $upc = $upc . Validators::validate_upce($upc, true);
+        }
+
+        if (strlen($upc)>8||strlen($upc)<8) {
+            return false;
+        }
+
+        if (!preg_match("/^0/", $upc)) {
+            return false;
+        }
+
+        if (Validators::validate_upce($upc)===false) {
+            return false;
+        }
+
+        if (preg_match("/0(\d{5})([0-3])(\d{1})/", $upc, $upc_matches)) {
+            $upce_test = preg_match("/0(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})/", $upc, $upc_matches);
+            if ($upce_test==false) {
+                return false;
+            }
+
+            if ($upc_matches[6]==0) {
+                $upce = "0".$upc_matches[1].$upc_matches[2].$upc_matches[6]."0000".$upc_matches[3].$upc_matches[4].$upc_matches[5].$upc_matches[7];
+            }
+
+            if ($upc_matches[6]==1) {
+                $upce = "0".$upc_matches[1].$upc_matches[2].$upc_matches[6]."0000".$upc_matches[3].$upc_matches[4].$upc_matches[5].$upc_matches[7];
+            }
+
+            if ($upc_matches[6]==2) {
+                $upce = "0".$upc_matches[1].$upc_matches[2].$upc_matches[6]."0000".$upc_matches[3].$upc_matches[4].$upc_matches[5].$upc_matches[7];
+            }
+
+            if ($upc_matches[6]==3) {
+                $upce = "0".$upc_matches[1].$upc_matches[2].$upc_matches[3]."00000".$upc_matches[4].$upc_matches[5].$upc_matches[7];
+            }
+        }
+
+        if (preg_match("/0(\d{5})([4-9])(\d{1})/", $upc, $upc_matches)) {
+            preg_match("/0(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})/", $upc, $upc_matches);
+
+            if ($upc_matches[6]==4) {
+                $upce = "0".$upc_matches[1].$upc_matches[2].$upc_matches[3].$upc_matches[4]."00000".$upc_matches[5].$upc_matches[7];
+            }
+
+            if ($upc_matches[6]==5) {
+                $upce = "0".$upc_matches[1].$upc_matches[2].$upc_matches[3].$upc_matches[4].$upc_matches[5]."0000".$upc_matches[6].$upc_matches[7];
+            }
+
+            if ($upc_matches[6]==6) {
+                $upce = "0".$upc_matches[1].$upc_matches[2].$upc_matches[3].$upc_matches[4].$upc_matches[5]."0000".$upc_matches[6].$upc_matches[7];
+            }
+
+            if ($upc_matches[6]==7) {
+                $upce = "0".$upc_matches[1].$upc_matches[2].$upc_matches[3].$upc_matches[4].$upc_matches[5]."0000".$upc_matches[6].$upc_matches[7];
+            }
+
+            if ($upc_matches[6]==8) {
+                $upce = "0".$upc_matches[1].$upc_matches[2].$upc_matches[3].$upc_matches[4].$upc_matches[5]."0000".$upc_matches[6].$upc_matches[7];
+            }
+
+            if ($upc_matches[6]==9) {
+                $upce = "0".$upc_matches[1].$upc_matches[2].$upc_matches[3].$upc_matches[4].$upc_matches[5]."0000".$upc_matches[6].$upc_matches[7];
+            }
+        }
+
+        return $upce;
+    }
+
     public static function convert_upca_to_ean13($upc) {
         if(!isset($upc) || !is_numeric($upc)) {
             return false;
--- src/Utilities/Validators.php
+++ src/Utilities/Validators.php
@@ -1,9 +1,133 @@
 <?php
 
+/*
+    The below BSD licensing statement is copied from the original works, and applies
+    to the logic used to generate the barcodes (everything within the 'create' function)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+    Copyright 2011-2012 Cool Dude 2k - http://idb.berlios.de/
+    Copyright 2011-2012 Game Maker 2k - http://intdb.sourceforge.net/
+    Copyright 2011-2012 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+    $FileInfo: validate.php - Last Update: 02/13/2012 Ver. 2.2.5 RC 1 - Author: cooldude2k $
+*/
+
 namespace CraigWoodlandOW\LaravelBarcodeGenerator\Utilities;
 
 class Validators
 {
+    function validate_upce($upc, $return_check=false)
+    {
+        if (!isset($upc)||!is_numeric($upc)) {
+            return false;
+        }
+
+        if (strlen($upc)>8) {
+            preg_match("/^(\d{8})/", $upc, $fix_matches);
+            $upc = $fix_matches[1];
+        }
+
+        if (strlen($upc)>8||strlen($upc)<7) {
+            return false;
+        }
+
+        if (!preg_match("/^0/", $upc)) {
+            return false;
+        }
+
+        $CheckDigit = null;
+        if (strlen($upc)==8&&preg_match("/^(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})/", $upc, $upc_matches)) {
+            preg_match("/^(\d{7})(\d{1})/", $upc, $upc_matches);
+            $CheckDigit = $upc_matches[2];
+        }
+
+        if (preg_match("/^(\d{1})(\d{5})([0-3])/", $upc, $upc_matches)) {
+            preg_match("/^(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})/", $upc, $upc_matches);
+
+            if ($upc_matches[7]==0) {
+                $OddSum = (0 + $upc_matches[3] + 0 + 0 + $upc_matches[4] + $upc_matches[6]) * 3;
+                $EvenSum = $upc_matches[2] + 0 + 0 + 0 + $upc_matches[5];
+            }
+
+            if ($upc_matches[7]==1) {
+                $OddSum = (0 + $upc_matches[3] + 0 + 0 + $upc_matches[4] + $upc_matches[6]) * 3;
+                $EvenSum = $upc_matches[2] + 1 + 0 + 0 + $upc_matches[5];
+            }
+
+            if ($upc_matches[7]==2) {
+                $OddSum = (0 + $upc_matches[3] + 0 + 0 + $upc_matches[4] + $upc_matches[6]) * 3;
+                $EvenSum = $upc_matches[2] + 2 + 0 + 0 + $upc_matches[5];
+            }
+
+            if ($upc_matches[7]==3) {
+                $OddSum = (0 + $upc_matches[3] + 0 + 0 + 0 + $upc_matches[6]) * 3;
+                $EvenSum = $upc_matches[2] + $upc_matches[4] + 0 + 0 + $upc_matches[5];
+            }
+        }
+
+        if (preg_match("/^(\d{1})(\d{5})([4-9])/", $upc, $upc_matches)) {
+            preg_match("/^(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})(\d{1})/", $upc, $upc_matches);
+
+            if ($upc_matches[7]==4) {
+                $OddSum = (0 + $upc_matches[3] + $upc_matches[5] + 0 + 0 + $upc_matches[6]) * 3;
+                $EvenSum = $upc_matches[2] + $upc_matches[4] + 0 + 0 + 0;
+            }
+
+            if ($upc_matches[7]==5) {
+                $OddSum = (0 + $upc_matches[3] + $upc_matches[5] + 0 + 0 + $upc_matches[7]) * 3;
+                $EvenSum = $upc_matches[2] + $upc_matches[4] + $upc_matches[6] + 0 + 0;
+            }
+
+            if ($upc_matches[7]==6) {
+                $OddSum = (0 + $upc_matches[3] + $upc_matches[5] + 0 + 0 + $upc_matches[7]) * 3;
+                $EvenSum = $upc_matches[2] + $upc_matches[4] + $upc_matches[6] + 0 + 0;
+            }
+
+            if ($upc_matches[7]==7) {
+                $OddSum = (0 + $upc_matches[3] + $upc_matches[5] + 0 + 0 + $upc_matches[7]) * 3;
+                $EvenSum = $upc_matches[2] + $upc_matches[4] + $upc_matches[6] + 0 + 0;
+            }
+
+            if ($upc_matches[7]==8) {
+                $OddSum = (0 + $upc_matches[3] + $upc_matches[5] + 0 + 0 + $upc_matches[7]) * 3;
+                $EvenSum = $upc_matches[2] + $upc_matches[4] + $upc_matches[6] + 0 + 0;
+            }
+
+            if ($upc_matches[7]==9) {
+                $OddSum = (0 + $upc_matches[3] + $upc_matches[5] + 0 + 0 + $upc_matches[7]) * 3;
+                $EvenSum = $upc_matches[2] + $upc_matches[4] + $upc_matches[6] + 0 + 0;
+            }
+        }
+
+        $AllSum = $OddSum + $EvenSum;
+        $CheckSum = $AllSum % 10;
+        if ($CheckSum>0) {
+            $CheckSum = 10 - $CheckSum;
+        }
+
+        if ($return_check==false&&strlen($upc)==8) {
+            if ($CheckSum!=$CheckDigit) {
+                return false;
+            }
+
+            if ($CheckSum==$CheckDigit) {
+                return true;
+            }
+        }
+
+        if ($return_check==true) {
+            return $CheckSum;
+        }
+
+        if (strlen($upc)==7) {
+            return $CheckSum;
+        }
+    }
+
     public static function validate_upca($upc, $return_check = false)
     {
         if(!isset($upc) || !is_numeric($upc)) {
